1db313578dea73c31b69e8177610fbc383252349
Lifeway2025 <joan@lifeway.es>
2025-09-02 17:25:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def voice():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))(os.environ.get("PORT", 5000)))
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
